a55ad985a253c1e22b6d24c93abdc11df8250bd6
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2020-05-19 01:46:03 +0200

    add comments

--- __init__.py
+++ __init__.py
@@ -1,6 +1,30 @@
-# based on Carlos Duarte in add-on (More Decks Stats and Time Left) https://ankiweb.net/shared/info/1556734708 
-#License        | GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
-
+# -*- coding: utf-8 -*-
+"""
+Beautify Anki
+an Addon for Anki
+Github (https://github.com/my-Anki/Beautify-Anki)
+Copyright (c) 2020 Shorouk Abdelaziz (https://shorouk.dev)
+"""
+#################################################################################
+# Beautify Anki n is released under GNU AGPL, version 3 or later                #
+# This Addon uses the following CSS and Javascript libraries                    #
+#                                                                               #
+# Acknowledgments                                                               #
+# This Addon uses the following CSS and Javascript libraries                    #
+#   - Materialize                                                               #
+#   - Animate.css                                                               #
+#   - plotly                                                                    #
+# The Statistics part in the Deck Browser is based on Carlos Duarte             #
+#  addon "More Decks Stats and Time Left" which is based on                     #
+#  Dmitry Mikheev code, in add-on "More decks overview stats"                   #
+#  and calumks code, in add-on "Deck Stats"                                     #
+#                                                                               #
+# The Statistics part in The Deck Overview pages is based on                    #
+#  Kazuwuqt addon "More Overview Stats 2.1 " which is based on                  #
+#  the More Overview Stats 2 add-on by Martin Zuther which is in turn based on  #
+#  "More Overview Stats" by Calumks                                             #
+#                                                                               #
+#################################################################################
 
 from .deck_browser import updateRenderingMethods
 from .deck_overview import updateRenderingDeckOverview 
--- deck_browser.py
+++ deck_browser.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Beautify Anki
+an Addon for Anki
+Github (https://github.com/my-Anki/Beautify-Anki)
+Copyright (c) 2020 Shorouk Abdelaziz (https://shorouk.dev)
+"""
+#################################################################################
+# Beautify Anki n is released under GNU AGPL, version 3 or later                #
+# This Addon uses the following CSS and Javascript libraries                    #
+#                                                                               #
+# Acknowledgments                                                               #
+# This Addon uses the following CSS and Javascript libraries                    #
+#   - Materialize                                                               #
+#   - Animate.css                                                               #
+#   - plotly                                                                    #
+# The Statistics part in the Deck Browser is based on Carlos Duarte             #
+#  addon "More Decks Stats and Time Left" which is based on                     #
+#  Dmitry Mikheev code, in add-on "More decks overview stats"                   #
+#  and calumks code, in add-on "Deck Stats"                                     #
+#                                                                               #
+# The Statistics part in The Deck Overview pages is based on                    #
+#  Kazuwuqt addon "More Overview Stats 2.1 " which is based on                  #
+#  the More Overview Stats 2 add-on by Martin Zuther which is in turn based on  #
+#  "More Overview Stats" by Calumks                                             #
+#                                                                               #
+#################################################################################
 
 from anki.errors import DeckRenameError
 from anki.hooks import wrap
